42b1b037217a328f9b6c742fb057209283c00492
Garry <garrymamen@gmail.com>
2021-11-26 14:36:50 +0700

    fitur 13 done

--- ta/src/main/java/apap/ta/restcontroller/MesinRestController.java
+++ ta/src/main/java/apap/ta/restcontroller/MesinRestController.java
@@ -1,5 +1,20 @@
 package apap.ta.restcontroller;
 
+import apap.ta.model.MesinModel;
+import apap.ta.service.MesinRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2")
 public class MesinRestController {
-    
+    @Autowired
+    private MesinRestService mesinRestService;
+
+    @GetMapping(value = "/list-mesin")
+    private List<MesinModel> retrieveListMesin() { return mesinRestService.retrieveListMesin(); }
+
 }
