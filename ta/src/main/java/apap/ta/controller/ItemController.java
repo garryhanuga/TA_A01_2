eb7694f94e58a396969848d17bd818ec42cbd454
Endriyani Rahayu <endriyanir@gmail.com>
2021-11-26 17:15:38 +0700

    DELETE restcontroller/ItemRestController

--- ta/src/main/java/apap/ta/controller/ItemController.java
+++ ta/src/main/java/apap/ta/controller/ItemController.java
@@ -1,22 +1,12 @@
 package apap.ta.controller;
 
-import apap.ta.model.RequestUpdateItemModel;
 import apap.ta.rest.ItemDetail;
-import apap.ta.restcontroller.ItemRestController;
-import apap.ta.restcontroller.RequestUpdateItemRestController;
 import apap.ta.service.ItemRestService;
-import apap.ta.service.ItemRestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 import reactor.core.publisher.Mono;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ItemController {
