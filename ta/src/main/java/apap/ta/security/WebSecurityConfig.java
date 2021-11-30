18d4e6135af8f625f5365053d5b972000fc99ecd
monalisavalenciamerauje <monalisavalenciamerauje@gmail.com>
2021-11-30 16:58:50 +0700

    fitur 1

--- ta/src/main/java/apap/ta/security/WebSecurityConfig.java
+++ ta/src/main/java/apap/ta/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
-    }
+   }
 
 
 
