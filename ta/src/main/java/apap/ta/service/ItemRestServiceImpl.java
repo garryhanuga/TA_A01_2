f28e3f6a8da17f26c5e466308582901ef2b9a0fd
Endriyani Rahayu <endriyanir@gmail.com>
2021-12-08 00:43:38 +0700

    implementasi fitur 6 - Menampilkan Item Detail dari Daftar Item

--- ta/src/main/java/apap/ta/service/ItemRestServiceImpl.java
+++ ta/src/main/java/apap/ta/service/ItemRestServiceImpl.java
@@ -1,10 +1,17 @@
 package apap.ta.service;
 
 import apap.ta.rest.ItemDetail;
+import apap.ta.rest.ListItemDetail;
 import apap.ta.rest.Setting;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -18,9 +25,17 @@ public class ItemRestServiceImpl implements ItemRestService{
     }
 
     @Override
-    public Mono<ItemDetail> getListItem() {
+    public Mono<ListItemDetail> getListItem() {
         return this.webClient.get()
                 .retrieve()
-                .bodyToMono(ItemDetail.class);
+                .bodyToMono(ListItemDetail.class);
+    }
+
+    @Override
+    public Mono<ItemDetail> getItem(String uuid) {
+        return this.webClient.get().uri("/"+uuid)
+        .retrieve()
+        .bodyToMono(ItemDetail.class);
+
     }
 }
