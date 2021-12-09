6c8359f64a30006da178b569fc3f55e5c61c6dc2
garryhanuga <81477472+garryhanuga@users.noreply.github.com>
2021-12-09 22:41:37 +0700

    Garry (#45)
    * test
    
    * add application-production.properties
    
    * add application-production.properties
    
    * add procfile

--- ta/src/main/java/apap/ta/security/WebSecurityConfig.java
+++ ta/src/main/java/apap/ta/security/WebSecurityConfig.java
@@ -34,22 +34,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
-//    @Bean
+    @Bean
     public BCryptPasswordEncoder encoder(){
-        return new BCryptPasswordEncoder();
+     return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("kijangSatu").password(encoder().encode("nasiGoreng"))
-                .roles("ADMIN");
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("kijangDua").password(encoder().encode("ayamGoreng"))
-                .roles("STAFF_GUDANG");
-    }
+    // @Autowired
+    // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.inMemoryAuthentication()
+    //             .passwordEncoder(encoder())
+    //             .withUser("kijangSatu").password(encoder().encode("nasiGoreng"))
+    //             .roles("ADMIN");
+    //     auth.inMemoryAuthentication()
+    //             .passwordEncoder(encoder())
+    //             .withUser("kijangDua").password(encoder().encode("ayamGoreng"))
+    //             .roles("STAFF_GUDANG");
+    // }
 
    
 
