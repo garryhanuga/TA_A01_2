0c0db4dbb861a2e21c6e27f9fb7a1492a6907042
Hanifa Ariana <hanifariana@gmail.com>
2021-12-10 17:53:28 +0700

    fix endpoint get sifactory

--- src/main/java/apap/ta/rest/Setting.java
+++ src/main/java/apap/ta/rest/Setting.java
@@ -4,6 +4,6 @@ public class Setting {
     final public static String requestUpdateItemUrl = "";
     final public static String itemUrl = "https://si-item.herokuapp.com/api/item";
     final public static String mesinUrl = "";
-    final public static String cabangUrl = "https://444ad093-5662-4985-9db6-c1f81c72c0d5.mock.pstmn.io/rest/listCabang";
+    final public static String cabangUrl = "https://si-retail-a1-3.herokuapp.com/api/v1/daftar-alamat-cabang";
 
 }
