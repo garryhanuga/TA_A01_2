eb44bc2431be2e907fe6faaefd5573216438c678
Endriyani Rahayu <90310081+endriyanira@users.noreply.github.com>
2021-12-24 14:08:48 +0700

    Endriyani3 (#70)
    * updaterequestitem
    
    * Update README.md
    
    * add authority for admin and manager
    
    * update authority for add new pegawai by only admin
    
    * benerin authorization tambah pegawai
    
    * ganti size button kembali
    
    * update view daftar-request
    
    * delete debug code
    
    * resolve problem
    
    * update update item
    
    * update daftar-produksi
    
    * update security config for any role
    
    * penyelesaian fitur propose item ke sibusiness
    
    Co-authored-by: Endriyani Rahayu <endriyanirahayu@Endriyanis-MacBook-Air.local>
    Co-authored-by: hanifariana <68933628+hanifariana@users.noreply.github.com>
    Co-authored-by: Hanifa Ariana <hanifariana@gmail.com>
    Co-authored-by: Endriyani Rahayu <endriyanirahayu@192.168.100.93>

--- src/main/java/apap/ta/service/ProposeItemRestServiceImpl.java
+++ src/main/java/apap/ta/service/ProposeItemRestServiceImpl.java
@@ -5,14 +5,22 @@ import apap.ta.rest.ListItemDetail;
 import apap.ta.rest.Setting;
 import apap.ta.model.ItemModel;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.transaction.Transactional;
 
@@ -28,17 +36,32 @@ public class ProposeItemRestServiceImpl implements ProposeItemRestService{
     }
 
     @Override
-    public ItemDetail proposeItem(String nama, int harga, int stok, Long kategori) {
-        MultiValueMap<String, Object> data = new LinkedMultiValueMap<>();
-        data.add("harga", harga);
-        data.add("kategori", kategori);
-        data.add("nama", nama);
-        data.add("stok", stok);
+    public void proposeItem(String nama, int harga, int stok, Long kategori, String cluster) {
+        System.out.println("masukk rest serviceimpl");
+        String url = "https://tk-apap-a01-1.herokuapp.com/api/item-factory";
 
-        return this.webClient.post()
-                .syncBody(data)
-                .retrieve()
-                .bodyToMono(ItemDetail.class).block();
+        // create an instance of RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        // set `content-type` header
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // set `accept` header
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        //request body parameter
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", nama);
+        data.put("price", harga);
+        data.put("stock", stok);
+        data.put("category", kategori);
+
+        // build the request
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(data, headers);
+
+        // send POST request
+        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
 
     }
 
